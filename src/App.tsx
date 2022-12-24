697af946c5bf53485b9c5ac0b434d7b354641a46
Ostrovskiy_D <ostrov2ostrov2@gmail.com>
2022-12-24 03:04:31 +0300

    Change app.tsx lesson 07

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,35 @@ import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
-        <ul>
-            <li>css</li>
-            <li>html</li>
-            <li>js</li>
-            <li>react</li>
-        </ul>
-    </div>
+     <div>
+         <Header />
+         <Technologies />
+     </div>
   );
 }
+
+const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    )
+}
+
+const Header = ()=> {
+    return (
+        <div>
+            <a href="#">Home</a>
+            <a href="#">New feed</a>
+            <a href="#">Messages</a>
+        </div>);
+
+}
+
 
 export default App;
