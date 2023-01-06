e4704fafb077cf2c72a6aa401c0b4adec7087eb5
Ostrovskiy_D <ostrov2ostrov2@gmail.com>
2023-01-06 16:24:59 +0300

    add grid to App.css

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,40 @@ import './App.css';
 
 function App()  {
   return (
-     <div className='app-wrapper'>
-        <header className='header'>
-            <img src='https://img.freepik.com/free-vector/branding-identity-corporate-vector-logo-a-design_460848-8717.jpg?w=2000'/>
-        </header>
-         <nav>
-             <div><a>Profile</a></div>
-             <div><a>Messages</a></div>
-             <div><a>News</a></div>
-             <div><a>Music</a></div>
-             <div><a>Settings</a></div>
-         </nav>
-         <div>
-             <img src="https://img.freepik.com/free-photo/empty-sea-and-beach-background_74190-1749.jpg"/>
-         </div>
-         <footer>
-
-         </footer>
-     </div>
+      <div className='app-wrapper'>
+          <header className='header'>
+              <img src='https://upload.wikimedia.org/wikipedia/commons/1/1e/RPC-JP_Logo.png'/>
+          </header>
+          <nav className='nav'>
+              <div><a>Profile</a></div>
+              <div><a>Messages</a></div>
+              <div><a>News</a></div>
+              <div><a>Music</a></div>
+              <div><a>Settings</a></div>
+          </nav>
+          <div className='content'>
+              <div>
+                  <img src="https://img.freepik.com/premium-vector/background-in-a-matrix-style-falling-random-numbers-green-is-dominant-color_257312-85.jpg?w=2000"/>
+              </div>
+              <div>
+                  ava + decription
+              </div>
+              <div>
+                  My posts
+                  <div>
+                      New post
+                  </div>
+                  <div>
+                      <div>
+                          Post 1
+                      </div>
+                      <div>
+                          Post 2
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
   );
 }
 
