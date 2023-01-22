6079d299de142d6e3518a0ec50bba22797f09a93
Ostrovskiy_D <ostrov2ostrov2@gmail.com>
2023-01-22 22:15:46 +0300

    refactoring
    bll and UI

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,17 +3,18 @@ import s from './Dialogs.module.css';
 import {NavLink} from "react-router-dom";
 
 
-type DialogItemPropsType ={
+type DialogItemPropsType = {
     name: string
-    id: string
+    id: number
+
 }
 
 
-const DialogItem = (props:DialogItemPropsType) => {
+const DialogItem = (props: DialogItemPropsType) => {
     let path = "/dialogs/" + props.id
 
     return (
-        <div className={s.dialog +' '+ s.active}>
+        <div className={s.dialog + ' ' + s.active}>
             <NavLink to={path}>{props.name}</NavLink>
         </div>
     )
@@ -21,7 +22,7 @@ const DialogItem = (props:DialogItemPropsType) => {
 };
 
 type MessagePropsType = {
-    text: string;
+    message:string
 }
 
 const Message = (props: MessagePropsType) => {
@@ -30,21 +31,33 @@ const Message = (props: MessagePropsType) => {
 
 
 const Dialogs = () => {
+
+    let dialogsData = [
+        {id: 1, name: 'Dimych'},
+        {id: 2, name: 'Andrey'},
+        {id: 3, name: 'Sveta'},
+        {id: 4, name: 'Sasha'},
+        {id: 5, name: 'Vicktor'},
+        {id: 6, name: 'Andrew'}]
+
+    let messagesData = [
+        {id: 1, message: 'Hi'},
+        {id: 2, message: 'How is your IT'},
+        {id: 3, message: 'yo'},
+        {id: 4, message: 'Yo'},
+        {id: 5, message: 'YO'}]
+
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name ={'Dimych'} id={'1'}/>
-                <DialogItem name ={'Andrey'} id={'2'}/>
-                <DialogItem name ={'Sveta'} id={'3'}/>
-                <DialogItem name ={'Sasha'} id={'4'}/>
-                <DialogItem name ={'Viktor'} id={'5'}/>
-                <DialogItem name ={'Valera'} id={'6'}/>
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+
             </div>
             <div className={s.messages}>
-                <Message text={'Hi'} />
-                <Message text={'How is your IT'} />
-                <Message text={'YO'} />
-                <Message text={'YO'} />
+                <Message message={messagesData[0].message}/>
+                <Message message={messagesData[1].message}/>
             </div>
         </div>
     )
