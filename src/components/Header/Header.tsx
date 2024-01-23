4921831da3979875daae2f06ea1fc2f2f8ef63e7
Ostrovskiy_D <ostrov2ostrov2@gmail.com>
2024-01-23 23:06:21 +0300

    add display pages logic

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,7 @@
-import React, {FC} from "react";
+import React from "react";
 import s from './Header.module.css';
 import {NavLink} from "react-router-dom";
 import {HeaderContainerPropsType} from "./HeaderContainer";
-
 
 
 export const Header = (props:HeaderContainerPropsType) => {
@@ -10,7 +9,9 @@ export const Header = (props:HeaderContainerPropsType) => {
             <header className={s.header}>
                 <img alt ="logo" src='https://upload.wikimedia.org/wikipedia/commons/1/1e/RPC-JP_Logo.png'/>
                 <div className={s.loginBlock}>
-                    {props.isAuth ? props.login : <NavLink to={'/login'}>Login</NavLink>}
+                    {props.isAuth
+                        ? <div> {props.login} - <button onClick={props.logout}>Log out</button> </div>
+                        : <NavLink to={'/login'}>Login</NavLink>}
                 </div>
             </header>
 
