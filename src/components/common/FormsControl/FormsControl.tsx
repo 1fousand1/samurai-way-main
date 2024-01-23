ded148b3d3825eb413d83403029d8ce9f6b52663
Ostrovskiy_D <ostrov2ostrov2@gmail.com>
2024-01-23 17:47:09 +0300

    Create form control and handmade Input and Textarea add css styles

---
+++ src/components/common/FormsControl/FormsControl.tsx
@@ -0,0 +1,37 @@
+import React, {DetailedHTMLProps, TextareaHTMLAttributes} from "react";
+import {WrappedFieldMetaProps} from "redux-form";
+import styles from './FormsControls.module.css'
+
+type DefaultInputPropsType = DetailedHTMLProps<TextareaHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement>
+type FormsControlType = {
+    input: DefaultInputPropsType
+    meta: WrappedFieldMetaProps
+    FormType: string
+}
+export const FormsControls: React.FC<FormsControlType> = ({
+                                                              input,
+                                                              meta,
+                                                              FormType,
+                                                              ...props
+                                                          }) => {
+    const error = meta.touched && meta.error
+    const finalInputName = `${styles.formControl} ${error ? styles.error : ''}`
+
+    return (
+        <div className={finalInputName}>
+            <FormType {...input} {...props}/>
+            {error && <div className={styles.errorMessage}>{meta.error}</div>}
+        </div>
+    )
+}
+export const Textarea: React.FC<FormsControlType> = ({...props}) => {
+        return (
+            <FormsControls {...props} FormType={"textarea"}/>
+        )
+}
+
+export const Input: React.FC<FormsControlType> = ({...props}) => {
+    return (
+        <FormsControls {...props} FormType={"input"}/>
+    )
+}
