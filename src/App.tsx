d2927044274c1cb8514a8362c4e0b2c58734801b
Ostrovskiy_D <ostrov2ostrov2@gmail.com>
2024-01-29 19:14:42 +0300

    refactor: Convert 'App' to class component with Redux, add initialization logic
    Refactored 'App' to a class component for Redux integration. Added 'componentDidMount' to dispatch 'initializeTC' for app initialization.
    
    Added conditional rendering for a preloader during initialization.
    
    Updated types and prop definitions.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,61 @@
 import React from 'react';
 import './App.css';
 import {Navbar} from "./components/Navbar/Navbar";
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter, Route, withRouter} from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Login from "./components/Login/Login";
+import {connect} from "react-redux";
+import {compose} from "redux";
+import {initializeTC} from "./redux/thunks/appThunk";
+import {ReduxStateType} from "./redux/redux-store";
+import {Preloader} from "./components/common/Preloader/Preloader";
 
 
-const App = () => {
 
-    return (
-        <BrowserRouter>
-            <div className='app-wrapper'>
-                <HeaderContainer/>
-                <Navbar/>
-                <div className='app-wrapper-content'>
-                    <Route path='/dialogs' render={() => <DialogsContainer/>}/>
-                    <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
-                    <Route path='/users' render={() => <UsersContainer/>}/>
-                    <Route path='/login' render={() => <Login/>}/>
-{/*                 <Route path='/news' render={() => <News/>}/>
-                    <Route path='/music' render={() => <Music/>}/>
-                    <Route path='/settings' component={() => <Settings/>}/>*/}
+
+class App extends React.Component<AppPropsType> {
+    componentDidMount() {
+        this.props.initialize()
+    }
+    render() {
+        if (!this.props.isInitialized) return <Preloader/>
+
+        return (
+            <BrowserRouter>
+                <div className='app-wrapper'>
+                    <HeaderContainer/>
+                    <Navbar/>
+                    <div className='app-wrapper-content'>
+                        <Route path='/dialogs' render={() => <DialogsContainer/>}/>
+                        <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
+                        <Route path='/users' render={() => <UsersContainer/>}/>
+                        <Route path='/login' render={() => <Login/>}/>
+                    </div>
                 </div>
-            </div>
-        </BrowserRouter>
-    );
+            </BrowserRouter>
+        );
+    }
 }
 
+type MapStateToPropsType = {
+    isInitialized: boolean
+    isAuth: boolean
+}
 
-export default App;
+type MapDispatchToProps = {
+    initialize: () => void
+}
+
+export type AppPropsType = MapStateToPropsType & MapDispatchToProps
+const mapStateToProps = (state: ReduxStateType):MapStateToPropsType => ({
+    isInitialized: state.app.isInitialized,
+    isAuth: state.auth.isAuth
+})
+
+
+export default compose<React.ComponentType>(
+    withRouter,
+    connect(mapStateToProps, {initialize: initializeTC}))(App)
