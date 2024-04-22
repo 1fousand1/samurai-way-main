9c493cc17fc7f3aa85029193436db1c0f61c3581
Ostrovskiy_D <ostrov2ostrov2@gmail.com>
2024-04-22 22:02:29 +0300

    feat: add captcha

--- src/redux/actions/authAction.ts
+++ src/redux/actions/authAction.ts
@@ -1,13 +1,15 @@
-import {AUTH_SET_USER_DATA} from "./actionTypes";
+import {AUTH_GET_CAPTCHA_URL, AUTH_SET_USER_DATA} from "./actionTypes";
 
 export type AuthType = {
     id: number | null
     email: string | null
     login: string | null
     isAuth: boolean
+    captchaUrl: string | null
 }
 
-export type AuthActionType = ReturnType<typeof setAuthUserDataAC>
+export type AuthActionType = ReturnType<typeof setAuthUserDataAC> | ReturnType<typeof getCaptchaUrlAC>
+
 
 export const setAuthUserDataAC = (id: number | null, email: string | null, login: string | null, isAuth: boolean) => ({
     type: AUTH_SET_USER_DATA,
@@ -18,4 +20,13 @@ export const setAuthUserDataAC = (id: number | null, email: string | null, login
         isAuth: isAuth
     }
 } as const )
+
+
+
+export const getCaptchaUrlAC = (captchaUrl: string) => ({
+    type: AUTH_GET_CAPTCHA_URL,
+    payload: {
+        captchaUrl
+    }
+} as const)
 
