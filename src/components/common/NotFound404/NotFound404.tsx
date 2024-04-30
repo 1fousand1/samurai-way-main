e12e51608837282dcc2ed78ef276aca2273c83ae
Ostrovskiy_D <ostrov2ostrov2@gmail.com>
2024-04-30 18:06:02 +0300

    feat: create 404 not found component

---
+++ src/components/common/NotFound404/NotFound404.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NotFound404 = () => {
+    return (
+        <div>
+            <h1>404 Not Found</h1>
+            <p>The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.</p>
+            <p>Go back</p>
+        </div>
+    );
+};
+
+export default NotFound404;
