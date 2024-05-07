fef6b6f576f16f516ae0383bdb2f28849e99b238
Ostrovskiy_D <ostrov2ostrov2@gmail.com>
2024-05-07 23:46:55 +0300

    init: add new page

---
+++ src/components/common/UnderDevelopmentPage/UnderDevelopmentPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const UnderDevelopmentPage = () => {
+    return (
+        <div>
+            <h1>We are Under Maintenance.</h1>
+            <p>Will be Back Soon!</p>
+        </div>
+    );
+};
+
