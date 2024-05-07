ccf04a9bfb78ccd7e5e728171d43623a9c9fbd8e
Ostrovskiy_D <ostrov2ostrov2@gmail.com>
2024-05-07 23:49:40 +0300

    feat: add chatPage to route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {Navbar} from "./components/Navbar/Navbar";
+import {Navbar} from "./components/Aside/Navbar/Navbar";
 import {Redirect, Route, Switch, withRouter} from "react-router-dom";
 import {lazy} from 'react';
 
@@ -12,9 +12,11 @@ import {ReduxStateType} from "./redux/redux-store";
 import {Preloader} from "./components/common/Preloader/Preloader";
 import {withSuspense} from "./hoc/withSuspense";
 import NotFound404 from "./components/common/NotFound404/NotFound404";
+import {ChatPage} from "./components/Chat/ChatPage/ChatPage";
 
 const DialogsContainer = lazy(() => import("./components/Dialogs/DialogsContainer"));
 const ProfileContainer = lazy(() => import("./components/Profile/ProfileContainer"));
+const ChatContainer = React.lazy(() => import("./components/Chat/ChatPage/ChatPage"))
 const LoginContainer = lazy(() => import("./components/Login/Login"))
 const UsersContainer = lazy(() => import("./components/Users/UsersContainer"))
 
@@ -35,11 +37,12 @@ class App extends React.Component<AppPropsType> {
                         <Switch>
                             <Route exact path="/" render ={()=> <Redirect to='/profile'/>}/>
                             <Route path='/dialogs' render={withSuspense(DialogsContainer)}/>
+                            <Route path='/chat' render={()=> <ChatPage/>}/>
                             <Route path='/profile/:userId?' render={withSuspense(ProfileContainer)}/>
                             <Route path='/users' render={withSuspense(UsersContainer)}/>
+
                             <Route path='/login' render={withSuspense(LoginContainer)}/>
                             <Route path='*' render={()=><NotFound404/>}/>
-
                         </Switch>
                     </div>
                 </div>
