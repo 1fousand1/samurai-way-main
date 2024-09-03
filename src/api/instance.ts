78fd3b6339091b7272dce5b61289e976369ce393
Ostrovskiy_Danila <119683894+DanilaOstrovskiy@users.noreply.github.com>
2024-09-04 00:58:05 +0300

    -minor changes

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const instance = axios.create({
     baseURL: "https://social-network.samuraijs.com/api/1.0/",
-    withCredentials: true,
+    /*withCredentials: true,*/
     headers: {
         "API-KEY": `${process.env.REACT_APP_API_KEY}`
     },
