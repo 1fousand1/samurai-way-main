424c05586aa7cb02149f106549094890f7f53423
Ostrovskiy_Danila <119683894+DanilaOstrovskiy@users.noreply.github.com>
2024-09-04 00:51:12 +0300

    -minor changes

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -4,8 +4,7 @@ export const instance = axios.create({
     baseURL: "https://social-network.samuraijs.com/api/1.0/",
     withCredentials: true,
     headers: {
-        "API-KEY": `c4b28fa4-046a-47ee-989e-c914f3dc0c23`,
-        Authorization: `Bearer ${process.env.REACT_APP_AUTH_TOKEN}`,
+        "API-KEY": `${process.env.REACT_APP_API_KEY}`
     },
 });
 
